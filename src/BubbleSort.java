cc79708642ac0cb5b5d9eda474e781e7b345d2f3
Abdul Rafi <arafi6624@bths.edu>
2019-01-07 23:17:29 -0500

    complete

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,20 +1,21 @@
 public class BubbleSort {
-    public static void swap(int[] arr, int i, int j){
+    public static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    public static void BubbleSort(int[] arr){
-        boolean sorted = true;
-        while (sorted){
-            for(int x = 0; x < arr.length-1; x++){
-                for (int a = 0; a <arr.length-x-1; a++){
-                if (arr[x] > arr[x+1]);
-                {
-                    swap(arr,x,x+1);
+    public static void BubbleSort(int[] arr) {
+        int[] arrTemp = arr;
+        int swaps = -1;
+        while (swaps != 0) {
+            swaps = 0;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                    swaps++;
                 }
             }
         }
     }
-}}
+}
